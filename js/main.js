d6b7b69786da9142e105a165b5a8ef8d7c9ec6b8
Alex-lab-design <73022364+Alex-lab-design@users.noreply.github.com>
2020-12-11 23:18:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $(function(){
         });$('.reviews__tab').on('click', function(event) {
             var id = $(this).attr('data-id');
                 $('.reviews__inner').find('.reviews__content-item').removeClass('active-tabe').hide();
-                $('.reviews__tabs').find('.reviews__tab').removeClass('active');
-                $(this).addClass('active');
-                $('#'+id).addClass('active-tab').fadeIn();
+                $('.reviews__tabs').find('.reviews__tab').removeClass('activee');
+                $(this).addClass('activee');
+                $('#'+id).addClass('active-tabe').fadeIn();
                 return false;
             });
 });
